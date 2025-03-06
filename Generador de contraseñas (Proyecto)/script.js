d87d38a2f168912213dbe5c3aa0c71b2f058906d
Emilio <EmilianoR117@outlook.com>
2025-03-05 18:40:11 -0700

    fix: Se añadieron comentarios al codigo y se corrigieron algunos detalles

--- Generador de contraseñas (Proyecto)/script.js
+++ Generador de contraseñas (Proyecto)/script.js
@@ -9,9 +9,8 @@ const campoContraseña = document.getElementById("box-password")
 const fuerzaContraseña = document.getElementById("indicator")
 
 
-
+// Se hace que el valor al lado de la barra y los colores que indican la fuerza de la contraseña cambien cuando esta se desplaza
 cantidad.textContent = rango.value;
-
 rango.addEventListener("input", function(){
     cantidad.textContent = rango.value;
     if(rango.value <= 12){
@@ -27,15 +26,16 @@ rango.addEventListener("input", function(){
 });
 
 generar.addEventListener("click", function (){
+    // Se limpia el campo cada que se genera una nueva contraseña
     campoContraseña.textContent = ""
+    // Declaracion de los strings con sus respectivos caracteres
     const mayusculas = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     const minusculas = "abcdefghijklmnopqrstuvwxyz";
     const numeros = "0123456789";
     const simbolos = "!@#$%^&*()_+[]{}|;:,.<>?";
     let contraseña = "";
     
-    let password = "";
-
+    
     // Crea un arreglo para almacenar los grupos de caracteres seleccionados
     const grupos = [];
     if (casillaMayusculas.checked) grupos.push(mayusculas);
@@ -43,16 +43,19 @@ generar.addEventListener("click", function (){
     if (casillaNumeros.checked) grupos.push(numeros);
     if (casillaSimbolos.checked) grupos.push(simbolos);
     
+    // Se valida primero que se seleccione almenos un grupo
     if (grupos.length === 0) {
       alert("Selecciona al menos un tipo de caracter");
     } else {
       for (let i = 0; i < rango.value; i++) {
+        // Se escoge aleatoriamente uno de los grupos disponibles
         const grupoSeleccionado = grupos[Math.floor(Math.random() * grupos.length)];
+        // Se selecciona un caracter aleatorio dentro del grupo
         const indiceRandom = Math.floor(Math.random() * grupoSeleccionado.length);
-        password += grupoSeleccionado.charAt(indiceRandom);
+        contraseña += grupoSeleccionado.charAt(indiceRandom);
       }
       
-      campoContraseña.textContent = password;
+      campoContraseña.textContent = contraseña;
     }
 });
 
